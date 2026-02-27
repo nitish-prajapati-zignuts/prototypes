c287b52f2112c90b4a7bc2f82ace1aa41ebec84f
Nitish <nitishprajapati180@gmail.com>
2026-02-27 17:48:11 +0530

    Adding AuthContext Changes

--- Styling/my-app/app/(tabs)/(todoStack)/todos/_layout.tsx
+++ Styling/my-app/app/(tabs)/(todoStack)/todos/_layout.tsx
@@ -1,13 +1,29 @@
-import { Stack } from "expo-router";
+import { useAuth } from "@/store/AuthContext/AuthContext";
+import { Redirect, router, Stack } from "expo-router";
+import { useEffect } from "react";
 
 
 export default function TodosStackLayout() {
+   const { user } = useAuth();
+
+  const isUserExists = !!user;
+
+  useEffect(() => {
+    if (!isUserExists) {
+      router.replace("/");
+    }
+  }, [isUserExists]);
+
+
+  if (!isUserExists) return null;
   return <Stack screenOptions={{headerShown:false,headerBackButtonDisplayMode:"minimal"}}>
-    <Stack.Screen name="[id]" options={{headerTitle:"Todo's"}}>
+   <Stack.Protected guard={isUserExists}>
+   <Stack.Screen name="[id]" options={{headerTitle:"Todo's"}}>
       
     </Stack.Screen>
     <Stack.Screen name="index" options={{headerTitle:"Todo's"}}>
 
     </Stack.Screen>
+   </Stack.Protected>
   </Stack>;
 }
